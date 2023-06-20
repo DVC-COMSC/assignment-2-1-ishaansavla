b8f40515e71614e2b56dd27a75e1b888e15e5f02
ishaansavla <ishaan.savla16@gmail.com>
2023-06-20 11:01:16 -0700

    Added Features

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 def main():
+
+    males = float(input("How many male students? "))
+    females = float(input("How many female students? "))
+
+    total = males + females
+    m_perc = format(float(males / total), '.2%')
+    f_perc = format(float(females / total),'.2%')
+
+    print("")
+    print(f'Males: {m_perc}')
+    print(f'Females: {f_perc}')
+    print("")
+
     """
     ##################################################
     Comlete your code here
@@ -11,6 +24,8 @@ def main():
     # Do not delete the return statement
     ########################################
     """
+
+
     return m_perc, f_perc
 
 
